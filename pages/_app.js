6a4b61f5624ebaa26e19086678d541ab739c4e0d
kauecdev <kaue_cavalcante.cnt@outlook.com>
2021-04-23 14:09:08 -0300

    feat: preloading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/global.css'
+import '../styles/global.css';
+import { Preloader, Placeholder } from 'react-preloading-screen';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Preloader>
+      <Component {...pageProps} />
+      <Placeholder className="preloader">
+        <img src="/assets/images/preloading.gif" alt="Loading..."/>
+      </Placeholder>
+    </Preloader>
+  )
 }
 
-export default MyApp
+export default MyApp;
